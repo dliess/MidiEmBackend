1753edadd5f143226cdd3678b1f8aaef36724ca5
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2021-02-09 11:58:59 +0000

    nexxxt

--- UIAdapter/capnzero/generator/cmake/capnzero_generate_cpp.cmake
+++ UIAdapter/capnzero/generator/cmake/capnzero_generate_cpp.cmake
@@ -1,23 +1,22 @@
 function(capnzero_generate_cpp SOURCES HEADERS PROTOCOL_DESCRIPTION_FILE)
-  set(_gRPC_PROTO_GENS_DIR ${CMAKE_CURRENT_BINARY_DIR})
+  set(_GEN_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
 
   set(${SOURCES})
   set(${HEADERS})
 
   get_filename_component(FIL_WLE ${PROTOCOL_DESCRIPTION_FILE} NAME_WLE) # File name without directory and last extension
 
-  set(GEN_CAPNP_FILE "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}.capnp")
-  file(TOUCH ${GEN_CAPNP_FILE})
+  set(GEN_CAPNP_FILE "${_GEN_OUTPUT_DIR}/${FIL_WLE}.capnp")
 
   add_custom_command(
     OUTPUT  "${GEN_CAPNP_FILE}"
-            "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Client.h"
-            "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Client.cpp"
-            "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Server.h"
-            "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Server.cpp"
+            "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Client.h"
+            "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Client.cpp"
+            "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Server.h"
+            "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Server.cpp"
 #    COMMAND "$<TARGET_FILE:capnzeroc>"
-    COMMAND python3 capnzeroc.py
-    ARGS  --outdir=${_gRPC_PROTO_GENS_DIR}
+    COMMAND python3 generator/capnzeroc.py
+    ARGS  --outdir=${_GEN_OUTPUT_DIR}
           ${PROTOCOL_DESCRIPTION_FILE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     COMMENT "Running capnzeroc generator script on ${PROTOCOL_DESCRIPTION_FILE}"
@@ -25,39 +24,33 @@ function(capnzero_generate_cpp SOURCES HEADERS PROTOCOL_DESCRIPTION_FILE)
   )
 
   find_package(CapnProto CONFIG REQUIRED)
-  set(CAPNPC_SRC_PREFIX ${CMAKE_CURRENT_BINARY_DIR})
 
-  if(TARGET capnp_tool)
-    set(CAPNP_EXECUTABLE capnp_tool)
-    GET_TARGET_PROPERTY(CAPNPC_CXX_EXECUTABLE capnpc_cpp CAPNPC_CXX_EXECUTABLE)
-    GET_TARGET_PROPERTY(CAPNP_INCLUDE_DIRECTORY capnp_tool CAPNP_INCLUDE_DIRECTORY)
-    list(APPEND tool_depends capnp_tool capnpc_cpp)
-  else()
-    message(FATAL "no TARGET capnp_tool")
+  if(NOT TARGET CapnProto::capnp_tool)
+    message(SEND_ERROR "No capnp_tool TARGET")
   endif()
-
-  set(include_path -I "${CAPNPC_SRC_PREFIX}" -I "${CAPNP_INCLUDE_DIRECTORY}")
-  set(output_base ${CMAKE_CURRENT_BINARY_DIR}/capnp)
-
+  if(NOT TARGET CapnProto::capnpc_cpp)
+    message(SEND_ERROR "No capnpc_cpp TARGET")
+  endif()
+  
+  GET_TARGET_PROPERTY(CAPNPC_CXX_EXECUTABLE CapnProto::capnpc_cpp CAPNPC_CXX_EXECUTABLE)
   add_custom_command(
-    OUTPUT "${output_base}.c++" "${output_base}.h"
-    COMMAND capnp_tool
+    OUTPUT "${GEN_CAPNP_FILE}.c++" "${GEN_CAPNP_FILE}.h"
+    COMMAND CapnProto::capnp_tool
     ARGS compile
         -o ${CAPNPC_CXX_EXECUTABLE}
-        --src-prefix ${CAPNPC_SRC_PREFIX}
-        ${include_path}
+        --src-prefix ${_GEN_OUTPUT_DIR}
         ${GEN_CAPNP_FILE}
-    DEPENDS "${GEN_CAPNP_FILE}" ${tool_depends}
-    COMMENT "Compiling Cap'n Proto schema ${schema_file}"
+    DEPENDS "${GEN_CAPNP_FILE}" CapnProto::capnp_tool CapnProto::capnpc_cpp
+    COMMENT "Compiling Cap'n Proto schema ${GEN_CAPNP_FILE}"
     VERBATIM
   )
 
-  list(APPEND ${SOURCES} "${CAPNP_GEN_SRCS}")
-  list(APPEND ${SOURCES} "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Client.cpp")
-  list(APPEND ${SOURCES} "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Server.cpp")
-  list(APPEND ${HEADERS} "${CAPNP_GEN_HDRS}")
-  list(APPEND ${HEADERS} "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Client.h")
-  list(APPEND ${HEADERS} "${_gRPC_PROTO_GENS_DIR}/${FIL_WLE}_Server.h")
+  list(APPEND ${SOURCES} "${GEN_CAPNP_FILE}.c++")
+  list(APPEND ${SOURCES} "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Client.cpp")
+  list(APPEND ${SOURCES} "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Server.cpp")
+  list(APPEND ${HEADERS} "${GEN_CAPNP_FILE}.h")
+  list(APPEND ${HEADERS} "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Client.h")
+  list(APPEND ${HEADERS} "${_GEN_OUTPUT_DIR}/${FIL_WLE}_Server.h")
 
   set(${SOURCES} ${${SOURCES}} PARENT_SCOPE)
   set(${HEADERS} ${${HEADERS}} PARENT_SCOPE)
