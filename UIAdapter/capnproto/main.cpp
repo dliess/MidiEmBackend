8b2145b3eed176e06c49ba976f6d346d8dad9855
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2021-04-02 12:09:55 +0000

    cpm find package midiemcapnzero

--- UIAdapter/capnproto/main.cpp
+++ UIAdapter/capnproto/main.cpp
@@ -2,14 +2,15 @@
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
 #include <capnp/serialize.h>
+#include <capnp/compat/json.h>
 #include <iostream>
 
 int main()
 {
     ::capnp::MallocMessageBuilder message;
     Person::Builder builder = message.initRoot<Person>();
-    builder.setName("Dollyyyyyyyyyyyyyy.,sdmf.ssssssssssssssssssssggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggddss");
-    builder.setEmail("Dollyyyyyyyyyyyyyy.,sdmf.ssssssssssssssssssssggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggddss");
+    builder.setName("Gogoooo");
+    builder.setEmail("gogoo@gmail.com");
 
     //::capnp::Text::Builder txtBuilder = builder.getName();
     auto arrOfArr = message.getSegmentsForOutput();
@@ -18,5 +19,8 @@ int main()
     //std::cout << txtBuilder.cStr() << "\n";
     //Person person;
     //Date date;
+    capnp::JsonCodec json;
+    kj::String encoded = json.encode(message.getRoot<Person>());
+    std::cout << encoded.cStr() << "\n";
     return 0;
 }
--- UIAdapter/capnzero/generator/toml2Json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import toml
-import json
-import sys
-
-descrfile = sys.argv[1]
-data = toml.load(descrfile)
-print(json.dumps(data, indent=2))
