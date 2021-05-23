66b9f34f327cb7adc1a64669d52571ae00a9c6d1
Daniel <liessdaniel415@gmail.com>
2021-05-23 19:52:18 +0000

    nexxt

--- Base/MusicDevice/MusicDeviceMeta.h
+++ Base/MusicDevice/MusicDeviceMeta.h
@@ -8,11 +8,12 @@ inline void to_json<base::musicDevice::MusicDevice>(
    nlohmann::json& j,
    const base::musicDevice::MusicDevice& obj)
 {
-    j["deviceId"] = meta::serialize(obj.deviceId());
     j["id"] = meta::serialize(obj.id());
-    if(obj.soundHandler) j["soundHandler"] = true;
-    if(obj.controllerHandler) j["controllerHandler"] = true;
-    if(obj.sequencer) j["sequencer"] = true;
+    j["deviceName"] = meta::serialize(obj.deviceId().deviceName);
+    j["portName"] = meta::serialize(obj.deviceId().portName);
+    j["soundHandler"] = static_cast<bool>(obj.soundHandler);
+    j["controllerHandler"] = static_cast<bool>(obj.controllerHandler);
+    j["sequencer"] = static_cast<bool>(obj.sequencer);
 }
 
 #endif
