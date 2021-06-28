0b5e10159aabbb20ab9dd07fd7f95e9c251019c9
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2021-06-28 20:59:25 +0000

    not working

--- Base/MusicDevice/MusicDeviceSequencer.h
+++ Base/MusicDevice/MusicDeviceSequencer.h
@@ -1,15 +1,22 @@
 #ifndef MUSIC_DEVICE_SEQUENCER_H
 #define MUSIC_DEVICE_SEQUENCER_H
 
+#include "Midi1Output.h"
+#include <memory>
+
 namespace base::musicDevice
 {
 
 class Sequencer
 {
 public:
+   void initMidiOutHandler(std::shared_ptr<midi::Midi1Output> pMidiOut) noexcept;
+
    void startSequencer() noexcept;
    void stopSequencer() noexcept;
-   bool masked{false};
+private:
+   std::shared_ptr<midi::Midi1Output> m_midiOutHandler;
+   bool m_masked{false};
 };
 
 } // namespace base::musicDevice
