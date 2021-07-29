7c5fc2b4c3b22965524eb1604a08e099ce35a52c
Zicsi-Liess <liessdaniel415@gmail.com>
2021-07-29 11:29:21 +0200

    added converted from uc

--- Scripts/usercamp_2_midiem.py
+++ Scripts/usercamp_2_midiem.py
@@ -40,7 +40,10 @@ with open(sys.argv[1]) as csv_file:
    csv_data = csv.DictReader(csv_file)
    componentNames = []
    for csv_row in csv_data:
-      data["manufacturer"] = csv_row['\ufeffmanufacturer']
+      if '\ufeffmanufacturer' in csv_row:
+         data["manufacturer"] = csv_row['\ufeffmanufacturer']
+      if 'manufacturer' in csv_row:
+         data["manufacturer"] = csv_row['manufacturer']
       data["productName"] = csv_row["device"]
       if not csv_row["section"] in componentNames:
          data["soundSection"]["engines"][0]["components"].append({"name" : csv_row["section"]})
@@ -59,6 +62,10 @@ with open(sys.argv[1]) as csv_file:
       valueRange = None
       midiParamId = None
       if csv_row["cc_msb"] != "":
+         if csv_row["cc_min_value"] == "":
+            csv_row["cc_min_value"] = "0"
+         if csv_row["cc_max_value"] == "":
+            csv_row["cc_max_value"] = "127"
          valueRange = int(csv_row["cc_max_value"]) - int(csv_row["cc_min_value"]) + 1
          if csv_row["cc_lsb"] == "":
             midiParamId = {".type": "MidiControlChange", "id" : csv_row["cc_msb"]}
@@ -67,25 +74,37 @@ with open(sys.argv[1]) as csv_file:
          else:
             midiParamId = {".type": "MidiControlChangeHighRes", "idMsb" : csv_row["cc_msb"], "idLsb" : csv_row["cc_lsb"]}
       elif csv_row["nrpn_msb"] != "" or csv_row["nrpn_lsb"] != "":
+         if csv_row["nrpn_min_value"] == "":
+            csv_row["nrpn_min_value"] = "0"
+         if csv_row["nrpn_max_value"] == "":
+            csv_row["nrpn_max_value"] = "127"
          valueRange = int(csv_row["nrpn_max_value"]) - int(csv_row["nrpn_min_value"]) + 1
          midiParamId = {".type": "MidiNRPN", "idMsb" : csv_row["nrpn_msb"], "idLsb" : csv_row["nrpn_lsb"]}
          if csv_row["nrpn_min_value"] != "0" or csv_row["nrpn_max_value"] != "127":
             param["source"]["midi"]["sourceValueRange"] = { "from": csv_row["nrpn_min_value"], "to": csv_row["nrpn_max_value"]}
 
-      if csv_row["usage"] != "":
-         param["source"]["sourceRanges"] = []
-         for usage in csv_row["usage"].split(";"):
-            range, name = usage.strip().split(":")
-            min = None
-            max = None
-            if "-" in range:
-               min,max = range.split("-")
-            else:
-               min = range
-               max = range
-            minVal = float((int(min.strip())) / valueRange)
-            maxVal = float((int(max.strip()) + 1) / valueRange)
-            param["source"]["sourceRanges"].append({"name": name.strip(), "range": {"from": minVal, "to": maxVal}})
+      if "usage" in csv_row:
+         if csv_row["usage"] != "":
+            param["source"]["sourceRanges"] = []
+            for usage in csv_row["usage"].split(";"):
+               range, name = usage.strip().split(":")
+               min = None
+               max = None
+               if "-" in range:
+                  min,max = range.split("-")
+               else:
+                  min = range
+                  max = range
+
+               minVal = None
+               maxVal = None
+               if "0b" in min:
+                  minVal = float((int(min.strip(), 2)) / valueRange)
+                  maxVal = float((int(max.strip(), 2) + 1) / valueRange)
+               else:
+                  minVal = float((int(min.strip())) / valueRange)
+                  maxVal = float((int(max.strip()) + 1) / valueRange)
+               param["source"]["sourceRanges"].append({"name": name.strip(), "range": {"from": minVal, "to": maxVal}})
 
       param["source"]["midi"]["id"] = midiParamId
 
@@ -95,5 +114,5 @@ with open(sys.argv[1]) as csv_file:
          param["type"] = "continous-bipolar"
 
       data["soundSection"]["engines"][0]["parameters"].append(param)
-      print("THE ROW: " + str(csv_row))
+      #print("THE ROW: " + str(csv_row))
    print(json.dumps(data, indent=2))
