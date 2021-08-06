c42c4999a8374ab4cc4c6d9caeb695c3a30e4fad
Zicsi-Liess <liessdaniel415@gmail.com>
2021-08-06 10:59:46 +0200

    extend install script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,6 +2,11 @@
 
 TOPDIR=$(git rev-parse --show-toplevel)
 
-sudo cp -v "$TOPDIR/build/MidiEmBackend" /usr/bin/
-sudo cp -v -r "$TOPDIR/MidiConfigs" /etc
-sudo cp -v "$TOPDIR/systemd/midiem-backend.service" /usr/lib/systemd/user
+INSTALL_ROOT_DIR=""
+if [[ -d $1 ]]; then
+    INSTALL_ROOT_DIR=$1
+fi
+
+sudo cp -v "$TOPDIR/build/MidiEmBackend" $INSTALL_ROOT_DIR/usr/bin/
+sudo cp -v -r "$TOPDIR/MidiConfigs" $INSTALL_ROOT_DIR/etc
+sudo cp -v "$TOPDIR/systemd/midiem-backend.service" $INSTALL_ROOT_DIR/usr/lib/systemd/user
