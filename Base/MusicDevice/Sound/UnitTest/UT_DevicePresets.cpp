a7a8de97b00c044cbd18b93473eb26fcf822f06f
Zicsi-Liess <liessdaniel415@gmail.com>
2021-12-07 14:58:57 +0000

    fix

--- Base/MusicDevice/Sound/UnitTest/UT_DevicePresets.cpp
+++ Base/MusicDevice/Sound/UnitTest/UT_DevicePresets.cpp
@@ -1,10 +1,28 @@
 #include <gtest/gtest.h>
 #include "DevicePresets.h"
 
+inline
+bool operator==(const base::musicDevice::sound::preset::LFOData& lhs, 
+                const base::musicDevice::sound::preset::LFOData& rhs)
+{
+   return lhs.amplitude == rhs.amplitude &&
+          lhs.frequency == rhs.frequency &&
+          lhs.waveform == rhs.waveform &&
+          lhs.multiplierExp == rhs.multiplierExp;
+}
+
+inline
+bool operator==(const base::musicDevice::sound::preset::ParameterData& lhs, 
+                const base::musicDevice::sound::preset::ParameterData& rhs)
+{
+   return lhs.commanded == rhs.commanded &&
+          lhs.lfoData == rhs.lfoData;
+}
+
 using namespace base::musicDevice::sound;
 using namespace base::musicDevice::sound::preset;
 
-TEST(DevicePresetSerializationTest, test)
+TEST(DevicePresetSerializationTest, serialize)
 {
    Preset preset;
    preset.slotOnDeviceIndex = 1;
@@ -16,4 +34,19 @@ TEST(DevicePresetSerializationTest, test)
    preset.parameters.push_back(ParameterData{0.0, LFOData{0.5, 0.0, lfo::Waveform::Sine, 0}});
    auto j = nlohmann::json(preset);
    ASSERT_EQ(j.dump(), "{\"category\":\"None\",\"genre\":\"None\",\"parameters\":[{\"commanded\":0.163},{\"commanded\":0.162,\"lfo\":{\"amplitude\":0.316,\"frequency\":0.9}},{\"commanded\":1.0,\"lfo\":{\"amplitude\":0.4,\"frequency\":0.816}},{\"commanded\":0.0}]}");
+}
+
+TEST(DevicePresetSerializationTest, deserialize)
+{
+   auto j = nlohmann::json::parse("{\"category\":\"None\",\"genre\":\"None\",\"parameters\":[{\"commanded\":0.163},{\"commanded\":0.162,\"lfo\":{\"amplitude\":0.316,\"frequency\":0.9}},{\"commanded\":1.0,\"lfo\":{\"amplitude\":0.4,\"frequency\":0.816}},{\"commanded\":0.0}]}");
+   auto preset = j.get<Preset>();
+   ASSERT_EQ(preset.slotOnDeviceIndex, std::nullopt);
+   ASSERT_EQ(preset.category, Category::None);
+   ASSERT_EQ(preset.genre, Genre::None);
+   ASSERT_EQ(preset.parameters.size(), 4);
+   EXPECT_NEAR(preset.parameters[0].commanded, 0.163, 0.0001);
+   EXPECT_NEAR(preset.parameters[0].lfoData.amplitude, 0.0, 0.0001);
+   EXPECT_NEAR(preset.parameters[0].lfoData.frequency, 0.0, 0.0001);
+   ASSERT_EQ(preset.parameters[0].lfoData.waveform, lfo::Waveform::Sine);
+   ASSERT_EQ(preset.parameters[0].lfoData.multiplierExp, 0);
 }
