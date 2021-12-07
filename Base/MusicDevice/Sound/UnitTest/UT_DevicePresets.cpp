6a27b1938054a4018b5ca64b0a09e1f0d3cbac68
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2021-12-07 08:32:22 +0100

    nexxt

--- Base/MusicDevice/Sound/UnitTest/UT_DevicePresets.cpp
+++ Base/MusicDevice/Sound/UnitTest/UT_DevicePresets.cpp
@@ -1,10 +1,16 @@
 #include <gtest/gtest.h>
 #include "DevicePresets.h"
 
+using namespace base::musicDevice::sound;
 using namespace base::musicDevice::sound::preset;
 
 TEST(DevicePresetSerializationTest, test)
 {
    Preset preset;
-   
+   preset.slotOnDeviceIndex = 1;
+   preset.category = Category::None;
+   preset.genre = Genre::None;
+   preset.parameters.emplace_back(0.0, LFOData{0.0, 1.0, lfo::Waveform::Sine, 0});
+   auto j = nlohmann::json(preset);
+
 }
