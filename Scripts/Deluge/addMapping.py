d77d2807aa5f535d9a97ac7f1fe97ccae9025c65
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2022-07-29 23:06:06 +0000

    next

--- Scripts/Deluge/addMapping.py
+++ Scripts/Deluge/addMapping.py
@@ -10,7 +10,9 @@ tree = ET.parse('Valentin.xml')
 song = tree.getroot()
 
 kitChannelNr = 1
+kitChannelNrMax = 12
 synthChannelNr = 13
+synthChannelNrMax = 16
 
 def createMidiKnobs(channelNr):
     mr = copy.deepcopy(midiKnobs.getroot())
@@ -22,15 +24,15 @@ def createMidiKnobs(channelNr):
 instruments = song.find("instruments")
 for kit in instruments.findall("kit"):
     ss = kit.find("soundSources")
-    for sound in ss.findAll("sound"):
-        if(kitChannelNr < synthChannelNr):
+    for sound in ss.findall("sound"):
+        if(kitChannelNr <= kitChannelNrMax):
             sound.append(createMidiKnobs(kitChannelNr))
             kitChannelNr = kitChannelNr + 1
 
 for sound in instruments.findall("sound"):
-    if(synthChannelNr <= 16):
+    if(synthChannelNr <= synthChannelNrMax):
         sound.append(createMidiKnobs(synthChannelNr))
         synthChannelNr = synthChannelNr + 1
 
-xmlstr = ET.tostring(tree, encoding='utf8', method='xml')
+xmlstr = ET.tostring(midiKnobs, encoding='utf8', method='xml')
 print(xmlstr)
