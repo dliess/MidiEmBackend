ae1080753f5bf31081c710fb6f1606f0e90ed4da
dliess <liessdaniel415@gmail.com>
2024-02-18 07:50:40 +0000

    nexxt

--- Base/Instruments/Loader/MelodicInstrumentModifier.cpp
+++ Base/Instruments/Loader/MelodicInstrumentModifier.cpp
@@ -175,19 +175,19 @@ MelodicInstrumentModifier::determineComponentEngineId(
 void MelodicInstrumentModifier::fillReferences(
     musicDevice::MusicDevice* pMusicDevice)
 {
-   for(int i = 0; i < m_rMelodicInstrument.m_parameters.size(); ++i)
-   {
-      auto& parameterData = m_rMelodicInstrument.m_parameters[i];
-      if (parameterData.has_value() && parameterData->pEngineDescr == nullptr)
+   std::ranges::for_each(m_rMelodicInstrument.m_parameters, [pMusicDevice](auto& parameterData){
+      if (parameterData.has_value() && 
+         parameterData->pEngineDescr == nullptr &&
+         parameterData->engineId.mdName == pMusicDevice->deviceId().deviceName())
       {
-         auto it = std::ranges::find_if(m_rMelodicInstrument.m_voices, [i, pMusicDevice](auto& voice){
-            return voice.components[i].has_value() && 
-            voice.components[i]->soundDeviceId == pMusicDevice->deviceId();
-         });
-         if(it != m_rMelodicInstrument.m_voices.end())
+         auto engine = safe_at(pMusicDevice->description()->soundSection->engines, 
+                              parameterData->engineId.engineIdx);
+         if(!engine.has_value())
          {
-            parameterData->pEngineDescr = pMusicDevice->description()->soundSection->engineBase(it->components[i]->sdVoiceIdx);
+            spdlog::error("Engine not found in MusicDeviceDescription");
+            return;
          }
+         parameterData->pEngineDescr = engine.value();
       }
-   }
+   });
 }
